f3280796688d303076bc1aa6ae20def1f28a5124
Fede lloves <fede.lloves@gmail.com>
2020-03-10 20:14:37 -0300

    primer commit

---
+++ src/main/java/com/codenotfound/primefaces/LoginBean.java
@@ -0,0 +1,55 @@
+package com.codenotfound.primefaces;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class LoginBean {
+
+  private String userName;
+  private String password;
+  
+  public LoginBean(){
+      System.out.println("Construyendo el bean");
+  }
+  
+  public String login(){
+      if("admin".equals(userName) && "123456".equals(password)){
+          return "welcome";
+      }else{
+          return "index";
+      }
+  }
+
+  /*
+
+  public String showGreeting() {
+    return "Hello " + firstName + " " + lastName + "!";
+  }
+
+  public void onClick(){
+    System.out.println("click ...");
+  }
+*/
+
+    public String getUserName() {
+        System.out.println("Username getter");
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        System.out.println("Username setter");
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        System.out.println("Password getter");
+        return password;
+    }
+
+    public void setPassword(String password) {
+        System.out.println("Password setter");
+        this.password = password;
+    }
+}
